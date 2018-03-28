ed9c712f5ecceadbe6850647ddb5ce77ea3751f9
ArthuurT <muctutur72@hotmail.fr>
2018-03-28 11:15:06 +0200

    code erreur

--- APIs/Weather.rb
+++ APIs/Weather.rb
@@ -18,12 +18,17 @@ class Weather
 		forData = HTTParty.get('http://api.openweathermap.org/data/2.5/forecast?APPID='+API_KEY+'&q='+city+'&units=metric&lang=fr').parsed_response
 		curData = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?APPID='+API_KEY+'&q='+city+'&units=metric&lang=fr').parsed_response
 
-		weather = {'temp' => curData['main']['temp'], 'text' => curData['weather'].first['description'], 'logo' => curData['weather'].first['icon']}
-		one = {'date' => forData['list'][8]['dt_txt'],'temp' => forData['list'][8]['main']['temp'], 'tempmin' => forData['list'][8]['main']['temp_min'], 'tempmax' => forData['list'][8]['main']['temp_max'], 'logo' => forData['list'][8]['weather'].first['icon']}
-		two = {'date' => forData['list'][16]['dt_txt'],'temp' => forData['list'][16]['main']['temp'], 'tempmin' => forData['list'][16]['main']['temp_min'], 'tempmax' => forData['list'][16]['main']['temp_max'], 'logo' => forData['list'][16]['weather'].first['icon']}
-		three = {'date' => forData['list'][24]['dt_txt'],'temp' => forData['list'][24]['main']['temp'], 'tempmin' => forData['list'][24]['main']['temp_min'], 'tempmax' => forData['list'][24]['main']['temp_max'], 'logo' => forData['list'][24]['weather'].first['icon']}
+		if !forData.has_value?('404')  || !curData.has_value?('404') then
 
-		return {'info' => [{'city' => city},weather,one,two,three]}
+			weather = {'temp' => curData['main']['temp'], 'text' => curData['weather'].first['description'], 'logo' => curData['weather'].first['icon']}
+			one = {'date' => forData['list'][8]['dt_txt'],'temp' => forData['list'][8]['main']['temp'], 'tempmin' => forData['list'][8]['main']['temp_min'], 'tempmax' => forData['list'][8]['main']['temp_max'], 'logo' => forData['list'][8]['weather'].first['icon']}
+			two = {'date' => forData['list'][16]['dt_txt'],'temp' => forData['list'][16]['main']['temp'], 'tempmin' => forData['list'][16]['main']['temp_min'], 'tempmax' => forData['list'][16]['main']['temp_max'], 'logo' => forData['list'][16]['weather'].first['icon']}
+			three = {'date' => forData['list'][24]['dt_txt'],'temp' => forData['list'][24]['main']['temp'], 'tempmin' => forData['list'][24]['main']['temp_min'], 'tempmax' => forData['list'][24]['main']['temp_max'], 'logo' => forData['list'][24]['weather'].first['icon']}
+			return {'info' => [{'city' => city},weather,one,two,three]}
+
+		else
+			return {'Error' => city + " not found"}
+		end
 	end
 
 end
