645b263b7eb062bde58898888ad938b2d6205516
ArthuurT <muctutur72@hotmail.fr>
2018-04-07 18:35:54 +0200

    Heure twitter

--- APIs/Twitter.rb
+++ APIs/Twitter.rb
@@ -34,6 +34,13 @@ class TwitterAPI
 
 		client.search(" ",geocode:code.to_s).take(100).each_with_index do |tweet,index|
 			if !"#{tweet.full_text}".include? "RT" then
+
+				days = "#{tweet.created_at}".split(" ")[0]
+				heure = "#{tweet.created_at}".split(" ")[1]
+				date = days.split("-")[2]
+				date << " "
+				date << getMonth(days.split("-")[1])
+
 				tw['tweetname'] = "#{tweet.user.name}"
 				tw['name'] = "#{tweet.user.screen_name}"
 				tw['tweetname'] = "#{tweet.user.name}"
@@ -41,7 +48,8 @@ class TwitterAPI
 				tw['text'] = "#{tweet.full_text}"
 				tw['like'] = "#{tweet.favorite_count}"
 				tw['retweet'] = "#{tweet.retweet_count}"
-				tw['date'] = "#{tweet.created_at}"
+				tw['date'] = date
+				tw['time'] = heure
 				tw['url_tweet'] = "#{tweet.uri}"
 				tw['url_user'] = "#{tweet.user.uri}"
 				hash["tweets"] << tw
@@ -50,6 +58,35 @@ class TwitterAPI
 			end
 		end
 		return hash
+	end
+
+	def self.getMonth(num)
+		case num
+		when "01"
+			return "Janvier"
+		when "02"
+			return "Fevrier"
+		when "03"
+			return "Mars"
+		when "04"
+			return "Avril"
+		when "05"
+			return "Mai"
+		when "06"
+			return "Juin"
+		when "07"
+			return "Juillet"
+		when "08"
+			return "Aout"
+		when "09"
+			return "Septembre"
+		when "10"
+			return "Octobre"
+		when "11"
+			return "Novembre"
+		when "12"
+			return "Decembre"
+		end
 	end
 end
 
@@ -67,4 +104,4 @@ end
 # 	puts "#{tweet.user.screen_name} : #{tweet.full_text}"
 # end
 
-#puts JSON.pretty_generate(TwitterAPI.getTweets(48.866667,2.333333))
+puts JSON.pretty_generate(TwitterAPI.getTweets(48.866667,2.333333))
