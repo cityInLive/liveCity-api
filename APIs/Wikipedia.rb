beec55e5d17193562c67d4a4c418208777d757de
Valentin Pelloin <valentinpelloin@gmail.com>
2018-04-09 00:00:27 +0200

    Ah !

--- APIs/Wikipedia.rb
+++ APIs/Wikipedia.rb
@@ -193,9 +193,9 @@ end
 
 #https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Paris_-_Eiffelturm_und_Marsfeld2.jpg/1164px-Paris_-_Eiffelturm_und_Marsfeld2.jpg
 
-rep = Wikipedia.getWikiInfo('Solesmes' , 'Sarthe')
+#rep = Wikipedia.getWikiInfo('Solesmes' , 'Sarthe')
 
-puts JSON.pretty_generate(rep)
+#puts JSON.pretty_generate(rep)
 #rep.each { |key, valeur|
 #  puts key + ":"
 #  p valeur
